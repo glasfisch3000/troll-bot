9bba7e0a86a1738879c34a59086d195f87587722
glasfisch3000 <jakob@danckwerts.net>
2023-10-24 21:35:37 +0200

    fixed sessionid.js

--- actions/sessionid.js
+++ actions/sessionid.js
@@ -1,3 +1,3 @@
 module.exports = () => {
-  ('0000000000000000' + Math.floor(Math.random() * 0xffff_ffff_ffff_ffff).toString(16).toUpperCase()).slice(-16)
+  return ('0000000000000000' + Math.floor(Math.random() * 0xffff_ffff_ffff_ffff).toString(16).toUpperCase()).slice(-16)
 }
