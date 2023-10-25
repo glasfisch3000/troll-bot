6416d3bbf5ed19ef4b996224038bb5dfd72b4d00
glasfisch3000 <jakob@danckwerts.net>
2023-10-25 10:32:03 +0200

    added amthor.filter

---
+++ actions/messageCreateFilters/amthor.filter.js
@@ -0,0 +1,42 @@
+const guildID = require(__dirname + "/../../ids.js").guildID
+const stickerID = "1166653231607320606"
+const patterns = [
+  "amthor",
+]
+
+module.exports = async (client, message, logger) => {
+  const { log, err } = logger("amthor.filter")
+
+  try {
+    let content = message.content.toLowerCase()
+
+    for(const pattern of patterns) {
+      log(`checking pattern '${pattern}'`)
+      if(content.includes(pattern)) {
+        log("pattern found, replying")
+
+        const guild = await client.guilds.cache.get(guildID) || await client.guilds.fetch(guildID)
+        if(!guild) {
+          log("guild not found")
+          break
+        }
+        
+        const sticker = await guild.stickers.fetch(stickerID)
+        if(!sticker) {
+          log("sticker not found")
+          break
+        }
+
+        let reply = {
+          content: "",
+          stickers: [sticker]
+        }
+        message.reply(reply)
+
+        break
+      }
+    }
+  } catch(error) {
+    err(error)
+  }
+}
