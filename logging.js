ca44b39e92ed72383ca55fe97c95204fffcafae5
glasfisch3000 <jakob@danckwerts.net>
2023-10-25 16:23:54 +0200

    changed logging back to stdout

--- logging.js
+++ logging.js
@@ -1,33 +1,17 @@
-const fs = require("fs").promises
-const id = require(__dirname + "/actions/sessionid.js")()
-
 module.exports = (environment) => {
   return {
-    log: async (message) => { await _log(new Date(), environment, "LOG", message) },
-    err: async (message) => { await _log(new Date(), environment, "ERROR", message) },
+    log: (message) => { _log(new Date(), environment, "LOG", message) },
+    err: (message) => { _log(new Date(), environment, "ERROR", message) },
     childLogger: (child) => { return module.exports((environment || []).concat([`${child}`])) },
   }
 }
 
-async function _log(date, environment, status, message) {
+function _log(date, environment, status, message) {
   var text = `(${id}) ${date.toISOString()} `
   for(const component of environment) {
     text = `${text}[${component}] `
   }
   text = `${text}${status || "LOG"}: ${message}\n`
 
-  await _print(text)
-}
-
-async function _print(data) {
-  try {
-    var file = await fs.open(__dirname + "/log.txt", "a")
-    if(!file) return
-
-    await file.appendFile(data)
-  } catch(error) {
-    console.error("logging: " + error)
-  } finally {
-    file?.close()
-  }
+  console.log(text)
 }
