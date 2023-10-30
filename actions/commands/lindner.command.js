38e87c6c4100d59290f77e2cef9546ec67316049
glasfisch3000 <jakob@danckwerts.net>
2023-10-30 15:55:10 +0100

    lindner quotes

---
+++ actions/commands/lindner.command.js
@@ -0,0 +1,40 @@
+const fs = require("fs").promises
+
+module.exports.name = "lindner"
+module.exports.callback = async (logger, client, interaction) => {
+  const { log, err, childLogger } = logger("lindner.command")
+
+  try {
+    log("received interaction")
+    log("reading quotes")
+
+    const quotes = await fs.readFile(__dirname + "/../../data/lindnerquotes.txt")
+
+    if(!quotes) {
+      err("no quotes found")
+      await interaction.reply({ content: "couldn't find any content" })
+      return
+    }
+
+    const lines = quotes.split(/\n/g)
+
+    if(!lines) {
+      err("quotes empty")
+      await interaction.reply({ content: "couldn't find any content" })
+      return
+    }
+
+    const line = lines[floor(Math.random()*lines.length)]
+
+    log("replying")
+    await interaction.reply({ content: line })
+  } catch(error) {
+    err(error)
+
+    try {
+      await interaction.reply({ content: "error. idfk, check the logs" })
+    } catch(error) {
+      err(error)
+    }
+  }
+}
---
+++ actions/interactionCreate.action.js
@@ -0,0 +1,45 @@
+const fs = require("fs").promises
+var commands = []
+
+module.exports = async (client, logger) => {
+  const { log, err, childLogger } = logger("interactionCreate.action")
+
+  try {
+    let commands = []
+
+    log("importing command files")
+    const files = await fs.readdir(__dirname + "/commands")
+
+    for(const file of files) {
+      log(`checking file ${file}`)
+      if(file.endsWith(".command.js")) {
+        log(`adding command file ${file}`)
+        commands.push(require(__dirname + "/commands/" + file))
+      }
+    }
+
+    log("adding client listener")
+    client.on("interactionCreate", async interaction => {
+      const child = childLogger("session-" + sessionID())
+
+      try {
+        child.log(`received interaction: "${interaction.id}"`)
+
+        if(!interaction.isChatInputCommand()) {
+          child.log("interaction is not a command")
+          return
+        }
+
+        for(const command of commands) {
+          if(interaction.commandName === command.name) await command.callback(child.childLogger, client, interaction)
+        }
+      } catch(error) {
+        child.err(error)
+      }
+    })
+
+    log("setup done")
+  } catch(error) {
+    err(error)
+  }
+}
